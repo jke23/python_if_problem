a8a3bae422eaf4555c732047410401716467f4ae
jke23 <wjdrkdms7656@naver.com>
2021-10-01 14:33:21 +0900

    solved_11047

--- problem_11047.py
+++ problem_11047.py
@@ -5,13 +5,12 @@ coins=[int(sys.stdin.readline().strip()) for i in range(a)]
 real_min_coin = 0
 
 def coin_cal(coins,money,min_coin):
-    global n
-    if money > 1 :
-        for n in range(len(coins)-1,-1,-1) :
+    if money > 0 :
+        for n in range(len(coins) - 1, -1, -1):
             if money >= coins[n] :
                 break
         min_coin += money//coins[n]
-        coin_cal(coins,money%coins[n],min_coin)
+        return coin_cal(coins,money%coins[n],min_coin)
     return min_coin
 
 
