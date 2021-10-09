b85bd96fef99a557398e2f1dfa0337247fbe5e63
jke23 <wjdrkdms7656@naver.com>
2021-10-09 20:26:25 +0900

    problem_2798
    time out cord

--- problem_2798.py
+++ problem_2798.py
@@ -6,6 +6,14 @@ card_sum=[]
 
 for n in range(0,a-2) :
     for i in range(n+1,a-1):
-        for x in range(n+2,a):
-            card_sum.append(card[n]+card[i]+card[x])
+        for x in range(i+1,a):
+            if card[n]+card[i]+card[x] not in card_sum :
+                card_sum.append(card[n]+card[i]+card[x])
+distance = 1000
+index_num = 0
+for y in range(0,len(card_sum)) :
+    if distance > abs(b-card_sum[y]) :
+        distance = abs(b - card_sum[y])
+        index_num=y
 
+print(card_sum[index_num])
