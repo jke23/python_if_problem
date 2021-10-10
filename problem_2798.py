cffa16da33b0fd6b130f83280f545f37ad1577ab
jke23 <wjdrkdms7656@naver.com>
2021-10-10 18:10:18 +0900

    problem_2798

--- problem_2798.py
+++ problem_2798.py
@@ -7,13 +7,12 @@ card_sum=[]
 for n in range(0,a-2) :
     for i in range(n+1,a-1):
         for x in range(i+1,a):
-            if card[n]+card[i]+card[x] not in card_sum :
+            if card[n]+card[i]+card[x] > b :
+                continue
+            else :
                 card_sum.append(card[n]+card[i]+card[x])
-distance = 1000
-index_num = 0
-for y in range(0,len(card_sum)) :
-    if distance > abs(b-card_sum[y]) :
-        distance = abs(b - card_sum[y])
-        index_num=y
 
-print(card_sum[index_num])
+card_sum.sort()
+
+print(card_sum[-1])
+
