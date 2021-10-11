341036f1755ad93d1c0ada3be1623188b232404f
jke23 <wjdrkdms7656@naver.com>
2021-10-11 20:08:59 +0900

    problem

--- problem_15649.py
+++ problem_15649.py
@@ -1,18 +1,17 @@
-def next_num(number,len_num,k):
-    for i in range(len_num-1,-1,-1) :
-        if num[i] < k :
-            num[i] = num[i]+1
-            return num
-        num[i] = 1
-    return num
+N, M = map(int, input().split())
+visited = [False] * N  # 탐사 여부 check
+out = []  # 출력 내용
 
-a, b = map(int,input().split())
-num=[1 for _ in range(b)]
-i=1
-n=0
-while i>0 :
-    if num[0] == num[1] == num[2] :
-        print(num[0],num[1],sep=' ')
-    num=next_num(num,b,a)
-    if num == [1 for _ in range(b)] :
-        break
+def solve(depth, N, M):
+    if depth == M:  # 탈출 조건
+        print(' '.join(map(str, out)))  # list를 str으로 합쳐 출력
+        return
+    for i in range(len(visited)):  # 탐사 check 하면서
+        if not visited[i]:  # 탐사 안했다면
+            visited[i] = True  # 탐사 시작(중복 제거)
+            out.append(i+1)  # 탐사 내용
+            solve(depth+1, N, M)  # 깊이 우선 탐색
+            visited[i] = False  # 깊이 탐사 완료
+            out.pop()  # 탐사 내용 제거d
+
+solve(0, N, M)
---
+++ problem_15650.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+out=[]
+
+def solve (depth,M,N,i) :
+    if depth == M :
+        print(' '.join(map(str, out)))
+        return
+    for n in range(i+1,N+1):
+        out.append(n)
+        solve(depth+1,M,N,n)
+        out.pop()
+
+for i in range(1,N+1) :
+    out.append(i)
+    solve(1,M,N,i)
+    out=[]
+
