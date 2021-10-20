ec0894eaf54c54647b40c243a99edf6b4b5410c2
jke23 <wjdrkdms7656@naver.com>
2021-10-20 20:58:37 +0900

    problem_4673_not_solved

--- problem_4673.py
+++ problem_4673.py
@@ -1,2 +1,9 @@
-def self_num():
-    
+def self_num(x=1):
+    X_list = [1,2,3,4,5,6,7,8,9]
+    while True
+        if x < 10 :
+            num = [i for i in range(10)]
+            for n in range(X_list) :
+                num.del(2n)
+            print(num)
+        elif x < 100 and x >= 10 :
