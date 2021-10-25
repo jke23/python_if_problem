c7b772b191696151cd1aedb3197ec45fcae293b4
jke23 <wjdrkdms7656@naver.com>
2021-10-25 20:56:20 +0900

    problem_10_25

--- problem_1193.py
+++ problem_1193.py
@@ -1,2 +1,17 @@
 a = int(input())
 
+i = 0
+num = 0
+while True :
+    i += 1
+    num += i
+    if num >= a :
+        num -= i
+        break
+
+n = a-num
+if i%2 == 0 :
+    print("{}/{}".format(n+1,i-n))
+else :
+    print("{}/{}".format(i-n,n+1))
+
