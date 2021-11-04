33d626a13c2a1181769cb8bd658fea9ddee3be97
jke23 <wjdrkdms7656@naver.com>
2021-11-04 16:29:40 +0900

    problem_11_04

--- test2.py
+++ test2.py
@@ -1,6 +1,10 @@
-while True :
-    Med_list = list(map(int,input().split()))
-    if bool(Med_list) is True :
-        print(Med_list[0]+Med_list[1])
-    else :
-        break
+input_star = [["*"*a]*a]
+
+def recursive_star(n, star) :
+    for x in range(0,int(n/3)) :
+        input_star[int(n*n/3+n/3+n*x+1) : int(n*n/3+(n/3)*2+n*x+1)] = " "
+        for i in range\
+
+                    (0,9):
+            for b in range(0,int(n/3)) :
+                star[n/3*x]
