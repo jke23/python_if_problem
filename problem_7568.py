10b9755fca4a86f2e3c2dbd0ab994f7ec99ec005
jke23 <wjdrkdms7656@naver.com>
2021-11-08 20:29:34 +0900

    problem_11_08

--- problem_7568.py
+++ problem_7568.py
@@ -1,6 +1,16 @@
 from sys import stdin
 i = int(input())
 
-human = [list(map(int,stdin.readline().split())for _ in range(i))]
+human = [list(map(int,stdin.readline().split()))for _ in range(i)]
+
+first = 0
+count = 0
 
 print(human)
+for x in range(i) :
+    for y in range(x,i+1):
+        if human[x][0] > human[y][0] :
+            if human[x][1] > human[y][1] :
+                first = x
+            else :
+                count += 1
