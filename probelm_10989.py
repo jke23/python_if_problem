e4b42bb76ef016d18c64bbba1656d0e10cf5f0b6
jke23 <wjdrkdms7656@naver.com>
2021-11-13 20:52:08 +0900

    problem_11_13

---
+++ probelm_10989.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+max_num = 10000000
+a = int(input())
+A = []
+for _ in range(a) :
+    i = int(input())
+    A.append(i)
+
+count = [0]*(max_num+1)
+countSum = [0]*(max_num+1)
+
+for i in range(0, a):
+    count[A[i]] += 1
+
+countSum[0] = count[0]
+for i in range(1, max_num+1):
+    countSum[i] = countSum[i-1]+count[i]
+
+B = [0]*(a+1)
+for i in range(a-1, -1, -1):
+    B[countSum[A[i]]] = A[i]
+    countSum[A[i]] -= 1
+
+
+for i in range(1,a+1):
+    print(B[i])
+
+
+
