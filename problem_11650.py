831e0fd707ae59dcaceecad1eca55b11c2514026
jke23 <wjdrkdms7656@naver.com>
2021-11-17 18:00:15 +0900

    problem_11_17

--- problem_11650.py
+++ problem_11650.py
@@ -2,4 +2,16 @@ from sys import stdin
 
 a = int(stdin.readline())
 x_y_list = [list(map(int,stdin.readline().split()))for _ in range(a)]
+final = [0]*a
 
+for x in range(a) :
+    count = 0
+    for y in range(a) :
+        if x_y_list[x][0] > x_y_list[y][0] :
+            count += 1
+        elif x_y_list[x][0] == x_y_list[y][0] :
+            if x_y_list[x][1] > x_y_list[y][1] :
+                count += 1
+    final[count] = x_y_list[x]
+for n in range(a):
+    print(" ".join(map(str,final[n])))
