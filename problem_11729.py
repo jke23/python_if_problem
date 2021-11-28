e0561a4138c9a9ce5eda780a67cee264287ecb0d
jke23 <wjdrkdms7656@naver.com>
2021-11-28 16:58:57 +0900

    problem_11_28

--- problem_11729.py
+++ problem_11729.py
@@ -1,12 +1,13 @@
-def hanoi_tower(n, start, end):
-    if n == 1:
-        print(start, end)
-        return
-    hanoi_tower(n-1, start, 6-start-end)
-    print(start, end)
-    hanoi_tower(n-1, 6-start-end, end)
-
-
 n = int(input())
-print(2 ** n - 1)
-hanoi_tower(n, 1, 3)
+def hanoi(n, a, b, c):
+    if n == 1:
+        print(a, c)
+    else:
+        hanoi(n - 1, a, c, b)
+        print(a, c)
+        hanoi(n - 1, b, a, c)
+sum = 1
+for i in range(n - 1):
+    sum = sum * 2 + 1
+print(sum)
+hanoi(n, 1, 2, 3)
